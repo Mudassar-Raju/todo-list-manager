dbba5e3d585308270218b9c1a21dbd60c91f0ac6
Mudassar_Iqbal <160554870+Mudassar-Raju@users.noreply.github.com>
2024-04-17 19:07:29 +0500

    todo-list-manager

---
+++ index.js
@@ -0,0 +1,22 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Do you want to add more in your todos ?",
+            default: "true"
+        }
+    ]);
+    todos.push(addTask.firstQuestion);
+    condition = addTask.secondQuestion;
+    console.log(todos);
+}
